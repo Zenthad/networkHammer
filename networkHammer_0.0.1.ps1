968581debd54ba5bacd73ead081253f97a170847
Thaddeus Pearson <thaddeus@imap.cc>
2020-04-12 00:33:26 -0600

    Create networkHammer_0.0.1.ps1
    testing

---
+++ networkHammer_0.0.1.ps1
@@ -0,0 +1,15 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2020 v5.7.174
+	 Created on:   	4/12/2020 12:31 AM
+	 Created by:   	zenthad
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+
+
